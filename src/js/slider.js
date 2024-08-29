c3debf2ab3d6ccceca2fcf135f5849f16cf99a26
David7878 <david7878.dmb@gmail.com>
2024-08-29 12:07:08 +0200

    Add: Slider

---
+++ src/js/slider.js
@@ -0,0 +1,14 @@
+
+const slider = new Swiper('.slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    // Navigation arrows
+  
+  });
+  
+  
