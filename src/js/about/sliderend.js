6437f562fa33cbd028ad4e53e74924e42012b5cc
David7878 <david7878.dmb@gmail.com>
2024-09-01 16:52:33 +0200

    End about

---
+++ src/js/about/sliderend.js
@@ -0,0 +1,14 @@
+
+const sliderend = new Swiper('.sliderend', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    // Navigation arrows
+  
+  });
+  
+  
